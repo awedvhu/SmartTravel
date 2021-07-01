5712cf5ee34840fc3cddbb356c647065e26e42ab
黃重皓 <sokve855@gmail.com>
2021-07-01 16:32:45 +0800

    add news.menu

---
+++ 未上傳網站/網頁/news_menu/blade.php/news_menu.blade.php
@@ -0,0 +1,130 @@
+    <div class="news_container">
+        <img src="1.png">
+        <h3>最新消息</h3>
+    </div>
+      
+    <div class="row row-cols-md-3 row-cols-md-3  g-4" id="news_menu">
+      <div class="col_group">
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more"> 
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col_group">
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+  
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col_group">
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+  
+        <div class="col">
+          <div class="card h-100 ">
+            <a href="#" class="card-link" title="read more">
+            <img src="1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">2021/07/01</p>
+              <h5 class="card-title">Card title</h5>
+            </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p>
+      <ul class="pagination justify-content-center">
+        <li class="page-item"><a class="page-link border-white " href="#"> < </a></li>
+        <li class="page-item active"><a class="page-link rounded-circle " href="#">1</a></li>
+        <li class="page-item"><a class="page-link rounded-circle" href="#">2</a></li>
+        <li class="page-item"><a class="page-link rounded-circle" href="#">3</a></li>
+        <li class="page-item"><a class="page-link border-white" href="#"> > </a></li>
+      </ul>
+    </p>
+      
