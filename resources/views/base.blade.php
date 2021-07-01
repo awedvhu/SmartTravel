8e684836e45d90cd46f8404cfe088001fb683799
awedvhu <s1082858@gm.pu.edu.tw>
2021-07-01 16:45:39 +0800

    v1.1

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/nav.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('css/body.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet" type="text/css" />
     <script src="{{ asset('js/body.js') }}" type="text/javascript"></script>
     <title>台北旅遊網</title>
 
@@ -20,7 +21,9 @@
     <div>
         @yield('main');
     </div>
-
+    <div>
+        @include('footer');
+    </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,48 @@
+<footer class="main footer">
+    <div class="footer-content">
+        <div class="footer_box">
+            <h3>活動資訊</h3>
+            <div class="name"> <a href="#">最新消息</a> </div>
+            <div class="name"> <a href="#">異動訊息</a> </div>
+            <div class="name"> <a href="#">活動展演</a> </div>
+            <div class="name"> <a href="#">熱門話題</a> </div>
+            <div class="name"> <a href="#">活動年曆</a> </div>
+            <div class="name"> <a href="#">熱門話題</a> </div>
+        </div>
+        <div class="footer_box">
+            <h3>玩轉台北</h3>
+            <div class="name"> <a href="#">你好台北</a> </div>
+            <div class="name"> <a href="#">社群看臺北</a> </div>
+            <div class="name"> <a href="#">臺北必遊</a> </div>
+            <div class="name"> <a href="#">套裝遊程</a> </div>
+            <div class="name"> <a href="#">你好台北</a> </div>
+            <div class="name"> <a href="#">熱門TAG</a> </div>
+        </div>
+        <div class="footer_box">
+            <h3>遊憩景點</h3>
+            <div class="name"> <a href="#">主題景點</a> </div>
+            <div class="name"> <a href="#">景點快搜</a> </div>
+            <div class="name"> <a href="#">分區搜尋</a> </div>
+            <div class="name"> <a href="#">景點地圖</a> </div>
+        </div>
+        <div class="footer_box">
+            <h3>美食購物</h3>
+            <div class="name"> <a href="#">主題美食</a> </div>
+            <div class="name"> <a href="#">美食快搜</a> </div>
+            <div class="name"> <a href="#">主題購物</a> </div>
+            <div class="name"> <a href="#">購物娛樂</a> </div>
+            <div class="name"> <a href="#">店家地圖</a> </div>
+        </div>
+        <div class="footer_box">
+            <h3>旅館住宿</h3>
+            <div class="name"> <a href="#">交通資訊</a> </div>
+            <div class="name"> <a href="#">旅客服務</a> </div>
+            <div class="name"> <a href="#">旅遊須知</a> </div>
+            <div class="name"> <a href="#">統計公告</a> </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        Copyright © 2020 Department of Information and Tourism, Taipei City Government. All rights reserved
+    </div>
+
+</footer>
